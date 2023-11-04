30fd6a1aae3bc3b9b69eb512ad7657dd31d99521
fawaidac <fwdachmd@gmail.com>
2023-11-04 12:54:27 +0700

    [done] verifikasi otp

--- lib/app/modules/otp/controllers/otp_controller.dart
+++ lib/app/modules/otp/controllers/otp_controller.dart
@@ -1,6 +1,15 @@
+import 'dart:convert';
+
+import 'package:cdc/app/modules/register/views/register_view.dart';
+import 'package:cdc/app/routes/app_pages.dart';
+import 'package:cdc/app/services/api_services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class OtpController extends GetxController {
+  FirebaseAuth auth = FirebaseAuth.instance;
   var code = "";
   var fullname = "";
   var email = "";
@@ -11,9 +20,11 @@ class OtpController extends GetxController {
   var nim = "";
   var prodi = "";
 
+  RxInt countdown = 30.obs;
+  RxBool canResend = false.obs;
+
   @override
   void onInit() {
-    // TODO: implement onInit
     super.onInit();
     var args = Get.arguments;
     fullname = args['fullname'];
@@ -24,6 +35,104 @@ class OtpController extends GetxController {
     nik = args['nik'];
     nim = args['nim'];
     prodi = args['kode_prodi'];
+
+    startCountdown();
   }
-  
+
+  void startCountdown() {
+    countdown.value = 30;
+    canResend.value = false;
+
+    Future.delayed(Duration(seconds: 1), () {
+      countdown.value--;
+      if (countdown.value > 0) {
+        startCountdown();
+      } else {
+        canResend.value = true;
+      }
+    });
+  }
+
+  Future<void> verifikasiOtp() async {
+    try {
+      PhoneAuthCredential credential = PhoneAuthProvider.credential(
+          verificationId: RegisterView.verify, smsCode: code);
+      await auth.signInWithCredential(credential);
+
+      await handleRegister(email, nik, fullname, pw, phone, alamat, nim, prodi);
+    } catch (e) {
+      Get.snackbar("Error", 'Kode Otp Salah', margin: const EdgeInsets.all(10));
+    }
+  }
+
+  Future<void> handleRegister(
+    String email,
+    String nik,
+    String fullname,
+    String password,
+    String telp,
+    String alamat,
+    String nim,
+    String prodi,
+  ) async {
+    try {
+      final response = await register(
+          email, nik, fullname, "0$telp", password, alamat, nim, prodi);
+      if (response['code'] == 201) {
+        Get.offNamed(Routes.LOGIN);
+        Get.snackbar("Success", response['message'],
+            margin: const EdgeInsets.all(10));
+      } else {
+        Get.snackbar("Error", response['message'],
+            margin: const EdgeInsets.all(10));
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  static Future<Map<String, dynamic>> register(
+      String email,
+      String nik,
+      String fullname,
+      String telp,
+      String password,
+      String alamat,
+      String nim,
+      String kode) async {
+    final Map<String, dynamic> body = {
+      'email': email,
+      'nik': nik,
+      'fullname': fullname,
+      'password': password,
+      'no_telp': telp,
+      'alamat': alamat,
+      'nim': alamat,
+      'prodi': kode,
+    };
+    final res = await http.post(
+        Uri.parse('${ApiServices.baseUrl}/auth/user/register'),
+        body: jsonEncode(body),
+        headers: {
+          'Content-Type': 'application/json',
+        });
+    final data = jsonDecode(res.body);
+    return data;
+  }
+
+  void sendOtpAgain() async {
+    try {
+      await auth.verifyPhoneNumber(
+        verificationCompleted: (PhoneAuthCredential authCredential) async {},
+        verificationFailed: (FirebaseAuthException e) {},
+        codeSent: (verificationId, forceResendingToken) {
+          RegisterView.verify = verificationId;
+          startCountdown();
+        },
+        codeAutoRetrievalTimeout: (verificationId) {},
+      );
+    } catch (e) {
+      print(e);
+    }
+  }
 }
--- lib/app/modules/otp/views/otp_view.dart
+++ lib/app/modules/otp/views/otp_view.dart
@@ -1,22 +1,116 @@
+import 'package:cdc/app/utils/app_colors.dart';
+import 'package:cdc/app/utils/app_fonts.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
+import 'package:pinput/pinput.dart';
 import '../controllers/otp_controller.dart';
 
 class OtpView extends GetView<OtpController> {
   const OtpView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: white,
       appBar: AppBar(
-        title: const Text('OtpView'),
-        centerTitle: true,
+        backgroundColor: white,
+        shadowColor: Colors.transparent,
+        automaticallyImplyLeading: false,
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Icon(
+            Icons.keyboard_arrow_left_rounded,
+            color: black,
+            size: 30,
+          ),
+        ),
       ),
-      body: const Center(
-        child: Text(
-          'OtpView is working',
-          style: TextStyle(fontSize: 20),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Verifikasi Otp",
+              style: AppFonts.poppins(fontSize: 24, color: black),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 10, 0, 50),
+              child: Text(
+                "Masukkan 6 digit kode yang dikirim di nomor +62 ${controller.phone} untuk verifikasi.",
+                style: AppFonts.poppins(fontSize: 12, color: black),
+              ),
+            ),
+            Pinput(
+              length: 6,
+              showCursor: true,
+              onChanged: (value) {
+                controller.code = value;
+              },
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(0, 50, 0, 45),
+              height: 48,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [second, first]),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onPressed: () {
+                  controller.verifikasiOtp();
+                },
+                child: Text(
+                  'Verifikasi',
+                  style: AppFonts.poppins(fontSize: 14, color: white),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Tidak menerima kode OTP ? ",
+                  style: AppFonts.poppins(fontSize: 12, color: black),
+                ),
+                InkWell(
+                  onTap: () {
+                    if (controller.canResend.value) {
+                      controller.sendOtpAgain();
+                    }
+                  },
+                  child: Text(
+                    "Kirim Ulang",
+                    style: AppFonts.poppins(
+                      fontSize: 12,
+                      color: controller.canResend.value ? first : Colors.grey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Obx(() {
+              return Center(
+                child: Text(
+                  "Harap tunggu kode dalam 00:${controller.countdown.toString().padLeft(2, '0')}s",
+                  style: AppFonts.poppins(fontSize: 12, color: controller.countdown > 0 ? grey : first),
+                ),
+              );
+            }),
+          ],
         ),
       ),
     );
