20d860d9431469ccbe7017c8d72367a92065ddb6
fawaidac <fwdachmd@gmail.com>
2023-11-12 18:06:14 +0700

    [done] configure fcm

---
+++ lib/app/services/notification_services.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotificationsServices {
+  static final FlutterLocalNotificationsPlugin notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  static void initialized() {
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+            android: AndroidInitializationSettings("@mipmap/logoblack"));
+    notificationsPlugin.initialize(initializationSettings);
+  }
+
+  static void showNotificationForeground(RemoteMessage message) {
+    final notificationsDetail = NotificationDetails(
+        android: AndroidNotificationDetails("com.jtinova.cdc", "Cdc-Online",
+            importance: Importance.max, priority: Priority.high));
+    int notificationId = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+    notificationsPlugin.show(notificationId, message.notification!.title,
+        message.notification!.body, notificationsDetail);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:cdc/app/modules/splash/bindings/splash_binding.dart';
+import 'package:cdc/app/services/notification_services.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
@@ -8,22 +10,102 @@ import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
+Future<void> _backgroundMessageHandler(RemoteMessage message) async {
+  // Notifikasi diterima saat aplikasi ditutup (terminated)
+  print(
+      "Notifikasi diterima saat aplikasi ditutup (terminated): ${message.notification?.title}");
+  print(
+      "Notifikasi diterima saat aplikasi ditutup (terminated): ${message.notification?.body}");
+  LocalNotificationsServices.showNotificationForeground(message);
+}
+
 void main() async {
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
   WidgetsFlutterBinding.ensureInitialized();
+  FirebaseMessaging.onBackgroundMessage(_backgroundMessageHandler);
+  LocalNotificationsServices.initialized();
   await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
-  runApp(
-    GetMaterialApp(
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  void configureFirebaseMessaging() {
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage? message) {
+      if (message != null) {
+        // Notifikasi diterima saat aplikasi dibuka dari background (terminated)
+        print(
+            "Notifikasi diterima saat aplikasi dibuka dari background (terminated): ${message.notification?.title}");
+        print(
+            "Notifikasi diterima saat aplikasi dibuka dari background (terminated): ${message.notification?.body}");
+        LocalNotificationsServices.showNotificationForeground(message);
+      }
+    });
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      // Notifikasi diterima saat aplikasi berjalan di foreground
+      print(
+          "Notifikasi diterima saat aplikasi berjalan di foreground: ${message.notification?.title}");
+      print(
+          "Notifikasi diterima saat aplikasi berjalan di foreground: ${message.notification?.body}");
+      LocalNotificationsServices.showNotificationForeground(message);
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      // Notifikasi diterima saat aplikasi dibuka dari background (background running)
+      print(
+          "Notifikasi diterima saat aplikasi dibuka dari background (background running): ${message.notification?.title}");
+      print(
+          "Notifikasi diterima saat aplikasi dibuka dari background (background running): ${message.notification?.body}");
+      LocalNotificationsServices.showNotificationForeground(message);
+      Get.toNamed(Routes.SPLASH);
+    });
+
+    FirebaseMessaging.onBackgroundMessage(_backgroundMessageHandler);
+  }
+
+  Future<void> fcm() async {
+    await FirebaseMessaging.instance.setAutoInitEnabled(true);
+  }
+
+  Future<void> _backgroundMessageHandler(RemoteMessage message) async {
+    // Notifikasi diterima saat aplikasi ditutup (terminated)
+    print(
+        "Notifikasi diterima saat aplikasi ditutup (terminated): ${message.notification?.title}");
+    print(
+        "Notifikasi diterima saat aplikasi ditutup (terminated): ${message.notification?.body}");
+    LocalNotificationsServices.showNotificationForeground(message);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    configureFirebaseMessaging();
+    fcm();
+    LocalNotificationsServices.initialized();
+    FirebaseMessaging.onBackgroundMessage(_backgroundMessageHandler);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
       title: "Application",
       debugShowCheckedModeBanner: false,
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
       initialBinding: SplashBinding(),
       builder: EasyLoading.init(),
-    ),
-  );
+    );
+  }
 }
