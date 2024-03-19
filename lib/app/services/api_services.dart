594b836bd70c521e7c050854ea67e298257bcf83
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2024-03-19 09:52:49 +0700

    master: fix to build boundle

--- lib/app/services/api_services.dart
+++ lib/app/services/api_services.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ApiServices {
-  static String _url = "192.168.1.32:8000";
+  // static String _url = "192.168.1.32:8000";
+  static String _url = "192.168.0.117";
   static const int _port = 8000;
 
   static TextEditingController ipAddressController =
